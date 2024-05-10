f1b5a36abd24a8863df8a76b70ccf65eeaf20b4f
hcaeryks <matheusll@id.uff.br>
2024-05-10 09:50:18 -0300

    perfected run script

--- runscript.bash
+++ runscript.bash
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-> ./data.txt
-
-for filename in ./bin/*; do
-    echo -n "$filename," >> ./data.txt
-    mpirun -N 8 --oversubscribe "$filename" 10000000  >> ./data.txt
-done
-
-for filename in ./bin/*; do
-    echo -n "$filename," >> ./data.txt
-    mpirun -N 8 --oversubscribe "$filename" 100000000  >> ./data.txt
-done
-
-for filename in ./bin/*; do
-    echo -n "$filename," >> ./data.txt
-    mpirun -N 8 --oversubscribe "$filename" 1000000000  >> ./data.txt
-done
-
-for filename in ./bin/*; do
-    echo -n "$filename," >> ./data.txt
-    mpirun -N 8 --oversubscribe "$filename" 10000000000  >> ./data.txt
+echo "file,threads,n,time" > ./data.txt
+for i in 2 4 8; do
+    for j in 10000000 100000000 1000000000 10000000000; do
+        for filename in ./bin/*; do
+            echo -n "${filename:6},$i,$j," >> ./data.txt
+            mpirun -N $i --oversubscribe "$filename" $j  >> ./data.txt
+        done
+    done
 done
