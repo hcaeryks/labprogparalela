53a6bafaf2de5b62d4299147a9e4474d477d688f
hcaeryks <matheusll@id.uff.br>
2024-05-12 17:14:20 -0300

    updated gitignore and restores runscript

--- runscript.bash
+++ runscript.bash
@@ -2,7 +2,7 @@
 
 echo "file,threads,n,time" > ./data.txt
 for i in 2 4 8; do
-    for j in 10000000 100000000 1000000000 10000000000; do
+    for j in 10000000 100000000 1000000000; do
         for filename in ./bin/*; do
             echo -n "${filename:6},$i,$j," >> ./data.txt
             mpirun -N $i --oversubscribe "$filename" $j  >> ./data.txt
